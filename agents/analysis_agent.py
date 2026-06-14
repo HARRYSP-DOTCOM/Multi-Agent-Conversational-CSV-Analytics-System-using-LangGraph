ee3244f82a75a43b2c4194dd8fd8d8143670fb3c
HARRYSP-DOTCOM <harryshibu111@email.com>
2026-06-14 18:51:08 +0530

    Update: commit workspace changes

--- agents/analysis_agent.py
+++ agents/analysis_agent.py
@@ -1,5 +1,43 @@
 from state.agent_state import AgentState
+from services.dataset_loader import DatasetLoader
+loader = DatasetLoader()
+datasets = loader.load_datasets()
 def analysis_agent(state: AgentState):
     print("\n=== ANALYSIS AGENT ===")
-    print("Waiting for analysis logic...")
-    return state
+    metric = (
+        state["parsed_query"]["metric"]
+    )
+
+    retrieval = (
+        state["retrieval_result"]
+    )
+
+    dataset_name = retrieval["table"]
+
+    entity_column = retrieval["column"]
+
+    entity_value = (
+        retrieval["resolved_entity"]
+    )
+    print("Dataset:", dataset_name)
+    print("Metric:", metric)
+    print("Entity:", entity_value)
+
+    df = datasets[dataset_name] 
+    filtered_df = df[
+        df[entity_column] == entity_value
+    ]
+    value = filtered_df[
+        metric
+    ].sum()
+
+    print("Computed Value:")
+    print(value)
+
+    state["analysis_result"] = {
+        "metric": metric,
+        "value": float(value),
+        "dataset": dataset_name
+    }
+    return state    
+
