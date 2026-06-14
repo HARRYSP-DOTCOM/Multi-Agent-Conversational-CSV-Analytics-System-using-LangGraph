ddf0ce320ad71b375ad8495f31c56613835d9ff4
HARRYSP-DOTCOM <harryshibu111@email.com>
2026-06-14 19:01:44 +0530

    Update: commit workspace changes

--- agents/response_agent.py
+++ agents/response_agent.py
@@ -1,5 +1,29 @@
 from state.agent_state import AgentState
 def response_agent(state: AgentState):
     print("\n=== RESPONSE AGENT ===")
-    print("Preparing final response...")
+    parsed = state["parsed_query"]
+    retrieval = state["retrieval_result"]
+    analysis = state["analysis_result"]
+    original_entity = parsed["entity"]
+    resolved_entity = (
+        retrieval["resolved_entity"]
+    )
+    metric = analysis["metric"]
+    value = analysis["value"]
+
+    formatted_value = (
+        f"{value:,.0f}"
+    )
+
+    response = (
+        f'I interpreted "{original_entity}" '
+        f'as "{resolved_entity}  ".'
+        f'The {metric} is '
+        f'{formatted_value}.'
+    )
+    print(response)        
+
+    state["final_response"] = (
+        response
+    )
     return state
